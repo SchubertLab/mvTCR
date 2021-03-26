3fa3f27122590fdae8d52b19dbcb9790ba895c2d
DeepOzean <yang.an@outlook.com>
2021-03-26 10:53:13 +0100

    add teacher forcing to BiGRU

--- tcr_embedding/models/bigru.py
+++ tcr_embedding/models/bigru.py
@@ -74,6 +74,7 @@ class BiGRUDecoder(nn.Module):
 		self.bidirectional = params['bidirectional']
 		self.num_seq_labels = num_seq_labels
 		self.input_dim = input_dim
+		self.teacher_forcing = params['teacher_forcing']
 
 		self.embedding = nn.Embedding(num_embeddings=num_seq_labels, embedding_dim=params['embedding_dim'], padding_idx=0)
 		self.hidden_state_layer = nn.Linear(input_dim, params['hidden_size'] * params['num_layers'])
@@ -90,20 +91,6 @@ class BiGRUDecoder(nn.Module):
 		self.output_layer = nn.Linear(params['hidden_size'], num_seq_labels)
 		# self.softmax = nn.Softmax(dim=-1)  # not needed using nn.CrossEntropyLoss
 
-	def step(self, input, hidden_state):
-		"""
-		A single forward step to predict the next token
-		:param input: torch.Tensor with shape [batch_size, seq_len]
-		:param hidden_state: torch.Tensor with shape [num_layers, batch_size, hidden_size]
-		:return: output, hidden_state
-		"""
-		embedding = self.embedding(input)
-
-		output, hidden_state = self.gru(embedding, hidden_state)
-		output = self.output_layer(output)
-		# output = self.softmax(output)  # not needed using nn.CrossEntropyLoss
-
-		return output, hidden_state
 
 	def forward(self, shared_hidden_state, gt_tcr_seq):
 		"""
@@ -113,22 +100,69 @@ class BiGRUDecoder(nn.Module):
 		:return:
 		"""
 		decoder_raw_output = []
-		decoder_label_output = []
 		batch_size, seq_len = gt_tcr_seq.shape
 
-		# Initialize first input and hidden state
-		input = gt_tcr_seq[:, 0].unsqueeze(1)  # start symbol
 		hidden_state = self.hidden_state_layer(shared_hidden_state)  # shape=[batch_size, num_layers*hidden_size]
 		hidden_state = hidden_state.reshape(batch_size, self.num_layers, self.hidden_size)  # shape=[batch_size, num_layers, hidden_size]
 		hidden_state = hidden_state.permute(1, 0, 2)  # switch batch and layer dim, shape=[num_layers, batch_size, hidden_size]
 		hidden_state = hidden_state.contiguous()  # permute returns a non-contiguous Tensor
+
+		use_teacher_forcing = True if torch.rand(1) < self.teacher_forcing else False
+		if use_teacher_forcing:
+			input = gt_tcr_seq[:, :-1]  # all timesteps except last one
+			embedding = self.embedding(input)
+
+			output, hidden_state = self.gru(embedding, hidden_state)
+			decoder_raw_output = self.output_layer(output)
+
+		else:
+			# Initialize first input and hidden state
+			input = gt_tcr_seq[:, 0].unsqueeze(1)  # start symbol
+			for step in range(seq_len-1):
+				output, hidden_state = self.step(input, hidden_state)
+				input = torch.argmax(output, dim=-1)  # only needs to be seq_len=1 as hidden_state contains the previous information
+
+				decoder_raw_output.append(output)
+			decoder_raw_output = torch.cat(decoder_raw_output, dim=1)
+
+		return decoder_raw_output
+
+	def step(self, input, hidden_state):
+		"""
+		A single forward step to predict the next token
+		:param input: torch.Tensor with shape [batch_size, seq_len]
+		:param hidden_state: torch.Tensor with shape [num_layers, batch_size, hidden_size]
+		:return: output, hidden_state
+		"""
+		embedding = self.embedding(input)
+		output, hidden_state = self.gru(embedding, hidden_state)
+		output = self.output_layer(output)
+		# output = self.softmax(output)  # not needed using nn.CrossEntropyLoss
+
+		return output, hidden_state
+
+	def predict(self, shared_hidden_state, gt_tcr_seq):
+		"""
+		Only feed in own prediction, don't use teacher forcing here
+		:param shared_hidden_state:
+		:param gt_tcr_seq:
+		:return:
+		"""
+		decoder_raw_output = []
+		batch_size, seq_len = gt_tcr_seq.shape
+
+		hidden_state = self.hidden_state_layer(shared_hidden_state)  # shape=[batch_size, num_layers*hidden_size]
+		hidden_state = hidden_state.reshape(batch_size, self.num_layers, self.hidden_size)  # shape=[batch_size, num_layers, hidden_size]
+		hidden_state = hidden_state.permute(1, 0, 2)  # switch batch and layer dim, shape=[num_layers, batch_size, hidden_size]
+		hidden_state = hidden_state.contiguous()  # permute returns a non-contiguous Tensor
+
+		# Initialize first input and hidden state
+		input = gt_tcr_seq[:, 0].unsqueeze(1)  # start symbol
 		for step in range(seq_len-1):
 			output, hidden_state = self.step(input, hidden_state)
-			input = torch.argmax(output, dim=-1)
+			input = torch.argmax(output, dim=-1)  # only needs to be seq_len=1 as hidden_state contains the previous information
 
 			decoder_raw_output.append(output)
-			decoder_label_output.append(input)
-
 		decoder_raw_output = torch.cat(decoder_raw_output, dim=1)
-		# decoder_label_output = torch.cat(decoder_label_output, dim=1)
-		return decoder_raw_output#, decoder_label_output
+
+		return decoder_raw_output
